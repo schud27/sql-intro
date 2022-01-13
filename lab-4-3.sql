4e82c15bb56d8829f169d9f182d9ab269cc0f6a9
schud27 <dan.schulte@kellogg.northwestern.edu>
2022-01-13 03:22:36 +0000

    completed lab 4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -11,7 +11,13 @@ SELECT
 teams.name
 ,players.first_name
 ,players.last_name
-,max(stats.home_run)
+,max(stats.home_runs)
 
 FROM players
 INNER JOIN stats
+ON players.id = stats.player_id
+INNER JOIN teams
+ON stats.team_id = teams.id
+
+WHERE teams.year = 2019
+;
